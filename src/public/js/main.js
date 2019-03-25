3749338fa6136bf5023666f6cbd698449d1cbe99
JhonPerez95 <jhonperez.j95@gmail.com>
2019-03-25 02:54:57 -0500

    ingresando user

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,13 +7,41 @@ $(function () {
     const $messageBox = $('#message');
     const $chat = $('#idChat');
 
-    // Capturar eventos
-    $messageForm.submit(function (e) {
-        e.preventDefault();  // Desactivar el evento de refrescar la pagina
-        // $messageBox.val(); Obtenemos el valor el texto
-        socket.emit('send message', $messageBox.val()); 
-        $messageBox.val('');
-    });
+        // Capturar eventos
+        $messageForm.submit(function (e) {
+            e.preventDefault();  // Desactivar el evento de refrescar la pagina
+            // $messageBox.val(); Obtenemos el valor el texto
+            socket.emit('send message', $messageBox.val()); 
+            $messageBox.val('');
+        });
+
+
+    // Obteniendo datos del containerUser
+    const $userForm = $('#userForm');
+    const $userName = $('#userName');
+    const $userError = $('#userError');
+
+    const $userNames = $('#userNames');
+        
+        // Capturando Eventos
+        $userForm.submit(function (e) {
+            e.preventDefault(); 
+            socket.emit('new user', $userName.val(), function (data) {
+                if (data) {
+                    $('#userContainer').hide(); // Oculta el formulario de Usuario
+                    $('#containerApp').show(); // Muestra el Chat
+                }else{
+                    $userError.html(`
+                        <div class="alert alert-danger" role="alert">
+                            Usuario ya existente !
+                        </div>
+                    `);               
+                }
+                $userName.val('');
+            });
+        });
+
+
 
     socket.on('new message',function(data) { // Recibe datos del servidor y los muestra
         $chat.append(data + '<br/>');
--- src/socket.js
+++ src/socket.js
@@ -1,10 +1,22 @@
 module.exports = function (io) {
+    let userNames = [];
+
     io.on('connection',  socket => {
         console.log('un nuevo user conectado');
             //io = Es todos los clientes conectados
             // Socket = a uno solo.
-         socket.on('send message', function(data) { // Recibe los datos
-             io.sockets.emit('new message', data); // devuelve a tods los clientes conectados
+        socket.on('send message', function(data) { // Recibe los datos
+            io.sockets.emit('new message', data); // devuelve a tods los clientes conectados
          });
+
+        socket.on('new user', (data, cb)=>{
+            if (userNames.indexOf(data) != -1) { // validamos que el usuario ya exista
+                cb(false);
+            }else{
+                cb(true);
+                socket.userName = data;
+                userNames.push(socket.userName);
+            }
+        } );
     });
 };
