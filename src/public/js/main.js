fa688edc77ff6ed954b2a961dedc921317431fcd
JhonPerez95 <jhonperez.j95@gmail.com>
2019-03-25 00:28:09 -0500

    Chat  - Cliente servidor

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,21 @@
-io();
+$(function () {
+
+    const socket = io();
+
+    // Obteniendo los elemento del DOM desde interfaz
+    const $messageForm = $('#message-form');
+    const $messageBox = $('#message');
+    const $chat = $('#idChat');
+
+    // Capturar eventos
+    $messageForm.submit(function (e) {
+        e.preventDefault();  // Desactivar el evento de refrescar la pagina
+        // $messageBox.val(); Obtenemos el valor el texto
+        socket.emit('send message', $messageBox.val()); 
+        $messageBox.val('');
+    });
+
+    socket.on('new message',function(data) { // Recibe datos del servidor y los muestra
+        $chat.append(data + '<br/>');
+    } );
+});
--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,10 @@
 module.exports = function (io) {
     io.on('connection',  socket => {
         console.log('un nuevo user conectado');
+            //io = Es todos los clientes conectados
+            // Socket = a uno solo.
+         socket.on('send message', function(data) { // Recibe los datos
+             io.sockets.emit('new message', data); // devuelve a tods los clientes conectados
+         });
     });
 };
