892ae59e497ed1d0bf843558053a87235597322a
JhonPerez95 <jhonperez.j95@gmail.com>
2019-03-25 19:49:35 -0500

    Se agg .ico

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -7,49 +7,54 @@ $(function () {
     const $messageBox = $('#message');
     const $chat = $('#idChat');
 
-        // Capturar eventos
-        $messageForm.submit(function (e) {
-            e.preventDefault();  // Desactivar el evento de refrescar la pagina
-            // $messageBox.val(); Obtenemos el valor el texto
-            socket.emit('send message', $messageBox.val()); 
-            $messageBox.val('');
-        });
-
-
     // Obteniendo datos del containerUser
     const $userForm = $('#userForm');
     const $userName = $('#userName');
     const $userError = $('#userError');
-
+    
+    // Obteniendo el Container Listar usuarios
     const $userNames = $('#userNames');
         
-        // Capturando Eventos
-        $userForm.submit(function (e) {
-            e.preventDefault(); 
-            socket.emit('new user', $userName.val(), data =>{
-                if (data) {
-                    $('#userContainer').hide(); // Oculta el formulario de Usuario
-                    $('#containerApp').show(); // Muestra el Chat
-                }else{
-                    $userError.html(`
-                        <div class="alert alert-danger" role="alert">
-                            Usuario ya existente !
-                        </div>
-                    `);         
-                }
-            });
-            $userName.val('');      
-        });
+    // CAPTURANDO EVENTOS
 
-    socket.on('userNames', data=>{ // Listando usuarios 
-        let html = '';
-       for (let i = 0; i < data.length; i++) {
-          html += ` <p> <i class="fas fa-user-ninja"> </i> ${data[i]}</p> `
-       }
-       $userNames.html(html);
+    $messageForm.submit(function (e) {
+        e.preventDefault();                     // Desactivar el evento de refrescar la pagina
+        //$messageBox.val(); Obtenemos el valor el texto
+        socket.emit('send message', $messageBox.val(),data=>{
+            $chat.append(`<p class="error">${data}</p>`);
+        }); 
+        $messageBox.val('');
     });
-    socket.on('new message',data =>{ // Recibe datos del servidor y los muestra
-        $chat.append('<strong>'+data.userName+':</strong>'+ data.msg +'<br/>');
-        
-    } );
+
+    $userForm.submit(function (e) {
+        e.preventDefault(); 
+        socket.emit('new user', $userName.val(), data =>{
+            if (data) {
+                $('#userContainer').hide();     // Oculta el formulario de Usuario
+                $('#containerApp').show();      // Muestra el Chat
+            }else{
+                $userError.html(`
+                    <div class="alert alert-danger" role="alert">
+                        Usuario ya existente !
+                    </div>
+                `);         
+            }
+        });
+        userName.val('');      
+    });
+
+
+    // CAPTURANDO EVENTOS ENVIADOS DESDE EL SERVIDOR
+
+    socket.on('userNames', data=>{                 // Listando usuarios 
+        let html = '';
+        for (let i = 0; i < data.length; i++) {
+            html += ` <p> <i class="fas fa-user-ninja"> </i> ${data[i]}</p> `
+        }
+        $userNames.html(html);
+    });
+
+    socket.on('new message',data =>{                // Recibe datos del servidor y los muestra
+        $chat.append('<strong>'+data.userName+': </strong>'+ data.msg +'<br/>');
+    });
 });
--- src/socket.js
+++ src/socket.js
@@ -1,34 +1,59 @@
 module.exports = function (io) {
-    let userNames = ['Luis', 'Carlos', 'Juan'];
+    let userNames = {};
 
     io.on('connection',  socket => {
         console.log('un nuevo user conectado');
-        socket.on('send message', function(data) { // Recibe los datos
-            io.sockets.emit('new message',{ // devuelve a tods los clientes conectados
-                msg: data,
-                userName: socket.userName
-            } ); 
-         });
 
+        socket.on('send message', (data, cb)=>{     // Recibe los datos del cliente
+            var msg = data.trim();
+
+            if (msg.substr(0,3)=== '/p ') {         // Se valida si es un mensaje privado
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if (index !== -1) {                 // Se valida que haya un mensaje para enviar privado
+                    var name = msg.substring(0, index);
+                    var msg = msg.substring(index +1 );
+                    if (name in userNames) {        // Se valida que el usuario exista
+                        userNames[name].emit('wisper', {
+                            msg: msg,
+                            userName: socket.userName
+                        });
+                    }else{
+                        cb('Error! Usuario no existe');
+                    }
+                }else{
+                    cb('Error! Por favor ingresa un mensaje');
+                }
+            }else{
+                io.sockets.emit('new message',{        // devuelve a tods los clientes conectados
+                    msg: data,
+                    userName: socket.userName
+                } ); 
+            }
+        });
+        // REGISTRO DE NUEVO USUARIO:
         socket.on('new user', (data, cb)=>{
             //console.log(data);
-            if (userNames.indexOf(data) != -1) { // validamos que el usuario ya exista
+            if (data in userNames) {                    // validamos que el usuario ya exista
                 cb(false);
             }else{
                 cb(true);
                 socket.userName = data;
-                userNames.push(socket.userName);
+                userNames[socket.userName]= socket;
                 updateUserName();
             }
         } );
-        socket.on('disconnect', data=>{ // Eliminando el usuario que se deconecte
+
+        // VALIDACION Y ELIMINACION DE USUARIO QUE SE DECONECTE:
+        socket.on('disconnect', data=>{                 // Eliminando el usuario que se deconecte
             if (!socket.userName) return;
-            userNames.splice(userNames.indexOf(socket.userName), 1); 
+            delete userNames[socket.userName]; 
             updateUserName();
         });
 
-        function updateUserName() {
-            io.sockets.emit('userNames', userNames);
+
+        function updateUserName() {                     // Funcion para act listado de clientes
+            io.sockets.emit('userNames', Object.keys(userNames));
         }
     });
 };
